760057401e7b79dba441b389263c2b35eb537e5b
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2020-01-06 00:06:51 +0300

    feat: add readFile and clean functions
    first v0.1.3 release
    
    [publish binary]

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,86 +1,45 @@
-#[macro_use]
-extern crate neon;
 extern crate svgcleaner;
 extern crate svgdom;
+extern crate neon;
+extern crate neon_serde;
+extern crate serde;
+
+mod options;
 
 use neon::prelude::*;
-use svgcleaner::{cleaner, ParseOptions, WriteOptions, CleaningOptions, StyleJoinMode};
-use svgdom::{
-    Indent,
-    ListSeparator,
-    AttributesOrder,
-};
+use svgcleaner::cleaner;
+use options::SvgCleanerOptions;
 
 
-fn compile(mut cx: FunctionContext) -> JsResult<JsString> {
-    let path = cx.argument::<JsString>(0)?;
+fn read_file(mut cx: FunctionContext) -> JsResult<JsString> {
+    let file_path = cx.argument::<JsString>(0)?.value();
 
-    let parse_opt = ParseOptions::default();
-    let write_opt = WriteOptions {
-        indent: Indent::None,
-        attributes_indent: Indent::None,
-        use_single_quote: false,
-        trim_hex_colors: true,
-        write_hidden_attributes: false,
-        remove_leading_zero: true,
-        use_compact_path_notation: true,
-        join_arc_to_flags: false,
-        remove_duplicated_path_commands: true,
-        use_implicit_lineto_commands: true,
-        simplify_transform_matrices: true,
-        list_separator: ListSeparator::Space,
-        attributes_order: AttributesOrder::Alphabetical,
-    };
-    let cleaning_opt = CleaningOptions {
-        remove_unused_defs: true,
-        convert_shapes: true,
-        remove_title: true,
-        remove_desc: true,
-        remove_metadata: true,
-        remove_dupl_linear_gradients: true,
-        remove_dupl_radial_gradients: true,
-        remove_dupl_fe_gaussian_blur: true,
-        ungroup_groups: true,
-        ungroup_defs: true,
-        group_by_style: true,
-        merge_gradients: true,
-        regroup_gradient_stops: true,
-        remove_invalid_stops: true,
-        remove_invisible_elements: true,
-        resolve_use: true,
+    match cleaner::load_file(&file_path) {
+        Ok(raw) => Ok(cx.string(raw)),
+        Err(err) => cx.throw_error(err.to_string()),
+    }
+}
 
-        remove_version: true,
-        remove_unreferenced_ids: true,
-        trim_ids: true,
-        remove_text_attributes: true,
-        remove_unused_coordinates: true,
-        remove_default_attributes: true,
-        remove_xmlns_xlink_attribute: true,
-        remove_needless_attributes: true,
-        remove_gradient_attributes: false,
-        join_style_attributes: StyleJoinMode::Some,
-        apply_transform_to_gradients: true,
-        apply_transform_to_shapes: true,
 
-        paths_to_relative: true,
-        remove_unused_segments: true,
-        convert_segments: true,
-        apply_transform_to_paths: true,
-
-        coordinates_precision: 6,
-        properties_precision: 6,
-        paths_coordinates_precision: 8,
-        transforms_precision: 8,
+fn clean(mut cx: FunctionContext) -> JsResult<JsString> {
+    let svg_file_body = cx.argument::<JsString>(0)?.value();
+    let options: SvgCleanerOptions = match cx.argument_opt(1) {
+        Some(arg) => {
+            let value = arg.downcast::<JsValue>().or_throw(&mut cx)?;
+            neon_serde::from_value(&mut cx, value)?
+        },
+        None => SvgCleanerOptions::default(),
     };
 
+    let (
+        parse_opt,
+        write_opt,
+        cleaning_opt,
+        flags
+    ) = options.split();
 
-    let raw = match cleaner::load_file(&path.value()) {
-        Ok(raw) => raw,
-        Err(err) => return cx.throw_error(err.to_string()),
-    };
-
-    let input_size = raw.len();
-    let mut buf = raw.into_bytes();
+    let input_size = svg_file_body.len();
+    let mut buf = svg_file_body.into_bytes();
     let mut prev_size = 0;
 
     loop {
@@ -92,17 +51,19 @@ fn compile(mut cx: FunctionContext) -> JsResult<JsString> {
         let mut doc = match cleaner::parse_data(std::str::from_utf8(&buf).unwrap(), &parse_opt) {
             Ok(d) => d,
             Err(e) => {
-                return cx.throw_error(e.to_string());
+                return cx.throw_error(e.to_string())
             }
         };
 
         // Clean document.
         match cleaner::clean_doc(&mut doc, &cleaning_opt, &write_opt) {
             Ok(_) => {}
-            Err(e) => {
+            Err(_) => {
                 break;
             }
         }
+
+
         // Clear buffer.
         //
         // We are reusing the same buffer for input and output data.
@@ -111,7 +72,12 @@ fn compile(mut cx: FunctionContext) -> JsResult<JsString> {
         // Write buffer.
         cleaner::write_buffer(&doc, &write_opt, &mut buf);
 
-        // If size is unchanged - exit from the loop.
+        if !flags.multipass {
+            // Do not repeat without '--multipass'.
+            break;
+        }
+
+//         If size is unchanged - exit from the loop.
         if prev_size == buf.len() {
             break;
         }
@@ -119,14 +85,25 @@ fn compile(mut cx: FunctionContext) -> JsResult<JsString> {
         prev_size = buf.len();
     }
 
-    if buf.len() > input_size {
-        return cx.throw_error("Error: cleaned file is bigger than original.");
+    if !flags.allow_bigger_file && buf.len() > input_size {
+        return cx.throw_error("Cleaned file is bigger than original.");
     }
 
-    Ok(cx.string(std::str::from_utf8(&buf).unwrap()))
+    // Optionally add a newline to the end of the file.
+    // This is placed after the check for if the file is smaller. It's OK if the
+    // file grows when adding a newline, since the user explicitly wanted that.
+    if flags.append_newline {
+        buf.push(b'\n');
+    }
+
+
+    let content = std::str::from_utf8(&buf).unwrap();
+    let js_content = cx.string(content);
+    Ok(js_content)
 }
 
 register_module!(mut cx, {
-    cx.export_function("compile", compile)?;
+    cx.export_function("readFile", read_file)?;
+    cx.export_function("clean", clean)?;
     Ok(())
 });
